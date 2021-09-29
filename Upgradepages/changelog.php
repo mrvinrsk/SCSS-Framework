13dc8ea6f71637b2ae0389d8a202fb2439df7d2e
Marvin <marvin.rosskothen@outlook.de>
2021-09-29 22:22:08 +0200

    updated changelog

--- Upgradepages/changelog.php
+++ Upgradepages/changelog.php
@@ -5,14 +5,39 @@
     <title>Upgrade - Changelog</title>
 
     <link rel="stylesheet" href="../assets/style/CSS/minified/Upgrade.min.css">
+    <link rel="stylesheet" href="../assets/style/CSS/minified/changelog.min.css">
+
     <script src="../assets/js/framework.js" defer></script>
+    <script src="https://kit.fontawesome.com/d2e7155937.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
 <?php include_once "./assets/elements/navigator.html"; ?>
 
-<main>
+<main class="container-md d-flex flex-d-column gy-3 gy-lg-6 py-2 py-lg-8">
+    <div class="wrapper bg-success" id="added">
+        <p class="heading-large fw-bold"><i class="fas fa-plus fsi-small"></i>Added</p>
 
+        <ul>
+            <li>Changelog is finally in use now.</li>
+        </ul>
+    </div>
+
+    <div class="wrapper bg-warning-dark" id="changed">
+        <p class="heading-large fw-bold"><i class="fas fa-pen fsi-small"></i>Changed</p>
+
+        <ul>
+            <li>Accordion Borders are now working as intended.</li>
+        </ul>
+    </div>
+
+    <div class="wrapper bg-error" id="removed">
+        <p class="heading-large fw-bold"><i class="fas fa-minus fsi-small"></i>Removed</p>
+
+        <ul>
+            <li>-</li>
+        </ul>
+    </div>
 </main>
 
 </body>
